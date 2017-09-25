58b4d9ed12766c9dd604e927ad3f90ad8804899c
Ata Tangban <atatangban@live.com>
2017-09-25 15:33:33 +0100

    loading sample data

--- Learn-Node-master/dang-thats-delicious/controllers/storeController.js
+++ Learn-Node-master/dang-thats-delicious/controllers/storeController.js
@@ -49,7 +49,7 @@ exports.resize = async (req, res, next) => {
 
 exports.createStore = async (req, res) => {
 	req.body.author = req.user._id;
-	const store = await new Store(req.body)).save();
+	const store = await new Store(req.body).save();
 	req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
 	res.redirect(`/store/${store.slug}`);
 };
